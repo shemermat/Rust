c49447fa6d14f5d3e2737c21283e7c65f65d80de
Matan Shemer <shemermat@gmail.com>
2018-05-19 17:16:56 +0300

    Hello_cargo

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-23180cf630047b3f.d
@@ -0,0 +1,5 @@
+C:\Users\sheme\git\Rust\projects\hello_cargo\target\debug\deps\hello_cargo-23180cf630047b3f.exe: src\main.rs
+
+C:\Users\sheme\git\Rust\projects\hello_cargo\target\debug\deps\hello_cargo-23180cf630047b3f.d: src\main.rs
+
+src\main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\sheme\git\Rust\projects\hello_cargo\target\debug\hello_cargo.pdb: C:\Users\sheme\git\Rust\projects\hello_cargo\src\main.rs
